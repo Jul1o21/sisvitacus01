16289eadecc609c66af89cf6331c8c2deb3918cd
Jul1o21 <julio.cubas@unmsm.edu.pe>
2024-06-14 03:25:32 -0500

    v1.44

--- app/src/main/java/com/example/sisvitag2/ui/view/LoginScreen.kt
+++ app/src/main/java/com/example/sisvitag2/ui/view/LoginScreen.kt
@@ -1,19 +1,19 @@
 package com.example.sisvitag2.ui.view
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.KeyboardArrowLeft
 import androidx.compose.material3.Button
 import androidx.compose.material3.Checkbox
@@ -22,9 +22,9 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
@@ -35,12 +35,16 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.example.sisvitag2.ui.theme.SisvitaG2Theme
-import com.example.sisvitag2.ui.theme.backgroundLight
 import com.example.sisvitag2.ui.viewmodel.LoginViewModel
+import androidx.compose.foundation.lazy.LazyColumn
 
 
 @Composable
-fun LoginScreen (navController: NavController) {
+fun LoginScreen (
+    navController: NavController,
+    loginViewModel: LoginViewModel = viewModel(), // Inicializa el LoginViewModel
+) {
+
     Column (
         modifier = Modifier
             .fillMaxSize()
@@ -73,32 +77,44 @@ fun TopBar() {
 
 @Composable
 fun Content() {
-    Column (
+    LazyColumn(
         modifier = Modifier
             .fillMaxWidth()
             .fillMaxHeight()
             .padding(30.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(
-            text = "Iniciar Sesión",
-            color = MaterialTheme.colorScheme.primary,
-            fontSize = 32.sp,
-            fontWeight = FontWeight.Bold,
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(top = 10.dp, bottom = 30.dp),
-            textAlign = TextAlign.Center
-        )
-        Formulario()
+        item {
+            Text(
+                text = "Iniciar Sesión",
+                color = MaterialTheme.colorScheme.primary,
+                fontSize = 32.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 10.dp, bottom = 30.dp),
+                textAlign = TextAlign.Center
+            )
+        }
+        item {
+            Formulario()
+        }
     }
 }
 
 @Composable
 fun Formulario() {
+
+    val loginViewModel: LoginViewModel = viewModel()
+
+    val correo: String by loginViewModel.correoState
+    val contrasenia: String by loginViewModel.contraseniaState
+
     Column(
         modifier = Modifier.fillMaxWidth()
     ) {
+
+        //Correo
         Text(
             text = "Correo",
             color = MaterialTheme.colorScheme.secondary,
@@ -109,9 +125,10 @@ fun Formulario() {
                 .padding(top = 5.dp, bottom = 10.dp),
             textAlign = TextAlign.Left
         )
+
         TextField(
-            value = "",
-            onValueChange = { it },
+            value = correo,
+            onValueChange = { loginViewModel.setEmail(it) },
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(bottom = 20.dp),
@@ -120,6 +137,8 @@ fun Formulario() {
             ),
             placeholder = { Text(text = "correo@dominio.com") }
         )
+
+        //Contraseña
         Text(
             text = "Contraseña",
             color = MaterialTheme.colorScheme.secondary,
@@ -131,8 +150,8 @@ fun Formulario() {
             textAlign = TextAlign.Left
         )
         TextField(
-            value = "",
-            onValueChange = { it },
+            value = contrasenia,
+            onValueChange = { loginViewModel.setPassword(it) },
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(bottom = 20.dp),
@@ -170,6 +189,7 @@ fun Formulario() {
             )
         }
     }
+
 }
 
 
--- app/src/main/java/com/example/sisvitag2/ui/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/example/sisvitag2/ui/viewmodel/LoginViewModel.kt
@@ -1,6 +1,38 @@
 package com.example.sisvitag2.ui.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.State
 
-class LoginViewModel: ViewModel() {
+class LoginViewModel : ViewModel() {
+
+
+    private val _correoState = mutableStateOf("")
+    val correoState: State<String> = _correoState
+
+    private val _contraseniadState = mutableStateOf("")
+    val contraseniaState: State<String> = _contraseniadState
+
+    private val _rememberMeState = mutableStateOf(false)
+    val rememberMeState: State<Boolean> = _rememberMeState
+
+    private val _isLoading = mutableStateOf(false)
+    val isLoading: State<Boolean> = _isLoading
+
+    fun setEmail(email: String) {
+        _correoState.value = email
+    }
+
+    fun setPassword(password: String) {
+        _contraseniadState.value = password
+    }
+
+    fun setRememberMe(checked: Boolean) {
+        _rememberMeState.value = checked
+    }
+
+    fun submitLoginForm() {
+
+    }
+
 }
